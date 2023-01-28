e832807929b8c6653518e7cf044a2dfae39d6933
queenanemone <shinhs9902@gmail.com>
2023-01-28 21:50:50 +0900

    ADD : Added boost function(when I push up arrow, it boosts)

--- CS Scripts/PlayerController.cs
+++ CS Scripts/PlayerController.cs
@@ -6,14 +6,33 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     [SerializeField] float torqueAmount = 1f;
+    SurfaceEffector2D surfaceEffector2D;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
-
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
